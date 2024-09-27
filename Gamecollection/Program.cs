f12b69698702b9226309cb89e140b8b2c1a1044a
Rlrasmussen <soede_rikke@hotmail.com>
2024-09-27 13:54:02 +0200

    Oprettelse af spillebrættet og bådene

--- Gamecollection/Program.cs
+++ Gamecollection/Program.cs
@@ -10,6 +10,108 @@ namespace Gamecollection
     {
         static void Main(string[] args)
         {
+            //Oprettelse af array brættet
+            string[,] mitBræt = new string[11, 11];
+            for (int x = 0; x < mitBræt.GetLength(0); x++)
+            {
+                for (int y = 0; y < mitBræt.GetLength(1); y++)
+                {
+                    mitBræt[x,y]= "_ ";
+                }
+            }
+            //Oprettelse af placering af bordet
+            string[] mitVertical = new string[] { "  ", "A ", "B ", "C ", "D ", "E ", "F ", "G ", "H ", "I ", "J " };
+            string[] mitHorisontal = new string[] { "  ", "1  ", "2  ", "3  ", "4  ", "5  ", "6  ", "7  ", "8  ", "9  ", "10 " };
+            for ( int i =0; i < mitBræt.GetLength(1); i++)
+            {
+                mitBræt[0, i] = mitVertical[i];
+                mitBræt[i, 0] = mitHorisontal[i];
+            }
+
+            //Oprettelse af de forskellige både
+            string[] båd2 = new string[] { "X ", "X " }; // 4 både af 2 længde
+            string[] båd3 = new string[] { "X ", "X ", "X " }; // 3 både af 3 længde
+            string[] båd4 = new string[] { "X ", "X ", "X ", "X " }; // 2 både af 4 længde
+            string[] båd5 = new string[] { "X ", "X ", "X ", "X ", "X " }; // 1 både af 5 længde
+
+
+            //Placering af båd
+            //Random random = new Random(); //Random placering af båden
+                                          //Båd 2
+            /*for (int i = 0; i < båd2.Length; i++)
+            {
+                
+                int randomNumber1 = random.Next(1, 9);
+                int randomNumber2 = random.Next(1, 9);
+
+                mitBræt[randomNumber1, randomNumber2 + i] = båd2[i];
+            } */
+
+            //Båd 3 
+            /*
+            for ( int j =0; j < 3; j++)
+            {
+                int randomNumber1 = random.Next(1, 9);
+                int randomNumber2 = random.Next(1, 9);
+                int randomTing = random.Next(0, 2);
+
+                if (randomTing == 0)
+                {
+                    for (int i = 0; i < båd3.Length; i++)
+                    {
+                        mitBræt[randomNumber1, randomNumber2 + i] = båd3[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < båd3.Length; i++)
+                    {
+                        mitBræt[randomNumber1 + i, randomNumber2] = båd3[i];
+                    }
+                }
+            } */
+
+
+            //Båd 5
+            /*
+            for (int j = 0; j < 1; j++)
+            {
+                int randomNumber1 = random.Next(1,4);
+                int randomNumber2 = random.Next(1,4);
+                int randomTing = random.Next(0, 2);
+
+                if (randomTing == 0)
+                {
+                    for (int i = 0; i < båd5.Length; i++)
+                    {
+                        mitBræt[randomNumber1, randomNumber2 + i] = båd5[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < båd5.Length; i++)
+                    {
+                        mitBræt[randomNumber1 + i, randomNumber2] = båd3[i];
+                    }
+                }
+            }
+            */
+
+            //Printe brættet
+            for (int x =0; x < mitBræt.GetLength(0); x++)
+            {
+                for (int y =0; y <mitBræt.GetLength(1); y++)
+                {
+                    Console.Write(mitBræt[x, y]);
+                }
+                Console.WriteLine(" ");
+            }
+
+
+
+            Console.ReadKey();
         }
+
+       
     }
 }
