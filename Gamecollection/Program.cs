905c6619eee8d74d38c337ec769e901f020297ce
Bjornlund <irenebjornlund@gmail.com>
2024-09-27 13:55:58 +0200

    intro
    Introtekst added
    Enum Kategori indeholder kategorier og værdier/beløb

--- Gamecollection/Program.cs
+++ Gamecollection/Program.cs
@@ -1,15 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Gamecollection
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+    enum Kategori { aBasisprogrammering, aSvar1, aSvar2, aSvar3, aSvar4, aSvar5, bFlowControl, bSvar1, bSvar2, bSvar3, bSvar4, bSvar5, cFunktioner, cSvar1, cSvar2, cSvar3, cSvar4, cSvar5, dArrays, dSvar1, dSvar2, dSvar3, dSvar4, dSvar5, eSpildesign, eSvar1, eSvar2, eSvar3, eSvar4, eSvar5, fVirksomhed, fSvar1, fSvar2, fSvar3, fSvar4, fSvar5 };
 }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Jeopardy, lavet af Irene
+
+        //string springOver = "Meld pas";
+
+        #region Intro
+        Console.WriteLine("Velkommen til Jeopardy! Hvor du stiller spørgsmålene og vi har Svarene!" + "\n" + "Vi har 6 katogorier med 5 beløb i hver. Gætter man rigtigt, må man vælge et nyt beløb indenfor samme kategori");
+
+        //Klassisk Jeopardy er med 3 spillere. 
+        Console.WriteLine("Spiller 1, input dit navn:");
+        string spillerEt = Console.ReadLine();
+
+        Console.WriteLine("Spiller 2, input dit navn:");
+        string spillerTo = Console.ReadLine();
+
+        Console.WriteLine("Spiller 3, input dit navn:");
+        string spillerTre = Console.ReadLine();
+
+        //Hvilken spiller starter, genereres med Random class
+        Random rng = new Random();
+        int randomNumber1 = rng.Next(0, 4);
+        Console.WriteLine($"Computeren vælger tilfældigt hvem der skal starte: Spiller {randomNumber1}");
+        Console.ReadKey();
+
+        #endregion
+
+        //Kategori Array
+        Console.WriteLine("Dagens kategorier er:");
+        //Kategori[] = new string[6, 6] {{ "aBasisprogrammering", "aSvar1", "aSvar2", "aSvar3", "aSvar4", "aSvar5" }, { "bFlowControl", "bSvar1", "bSvar2", "bSvar3", "bSvar4", "bSvar5"}, { "cFunktioner", "cSvar1", "cSvar2", "cSvar3", "cSvar4", "cSvar5"}, { "dArrays", "dSvar1", "dSvar2", "dSvar3", "dSvar4", "dSvar5"}, { "eSpildesign", "eSvar1", "eSvar2", "eSvar3", "eSvar4", "eSvar5"}, { "fVirksomhed", "fSvar1", "fSvar2", "fSvar3", "fSvar4", "fSvar5"} };
+
+        //Console.WriteLine(baseGrid(0, 6, 12, 18, 24, 36));
+
+        //Kald array
+        /*for (int x = 0; x < skakArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < skakArray.GetLength(1); y++)
+            {
+                Console.Write(skakArray[x, y] + " ");*/
+
+    
+
+        //breaker loops op så vi får rækker i boardet
+        Console.Write("\n");
+
+        while (randomNumber1 = ;)
+            {
+            Console.WriteLine("Input et bogstav for din valgte kategori og til hvilken værdi, du vil have:");
+            Console.WriteLine("Kategori:");
+            Console.ReadLine();
+            Console.WriteLine("Beløb:");
+            Console.ReadLine();
+
+            Console.WriteLine("Husk at ende dit spørgsmål med '?'");
+            Console.WriteLine("Korrekt! Du må vælge en ny kategori og en ny værdi");
+            }
+
+
+        #region Vinder / afslut
+        //Make into "highest"
+        /*int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+        for (int index = 0; index<array.Length; index++)
+        {
+        if (array[index] < currentSmallest)
+        currentSmallest = array[index];
+        }
+        Console.WriteLine(currentSmallest);*/
+        Console.WriteLine("Spiller X vinder med xxx point"); //køre array igennem for at finde højeste score
+        int[] array = new int[] {};
+
+        #endregion
+
+        Console.ReadKey();
+
+
+        
+
+
+
+    }
+    }
+
